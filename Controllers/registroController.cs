60779981237967e1175645d5bc0d998641b3353f
Neyser Tacanga <tacangan@gmail.com>
2021-04-27 19:32:05 -0500

    Intento de resolver

---
+++ Controllers/registroController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using practica2.Models;
+
+
+
+namespace practica2.Controllers
+{
+    public class registroController : Controller
+    {
+
+        
+    }
+}
---
+++ Models/registro.cs
@@ -0,0 +1,12 @@
+namespace practica2.Models
+{
+    public class RegistroUsuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DNI { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+    }
+}
